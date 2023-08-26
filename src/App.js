4e63a2ba9369d73a1fda6e407c42bcdda3cfe1ec
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-08-26 14:49:43 +0530

    commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import list from './list';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+      
+   setData(list)
+
+  },[])
+     
+    
+   
+    return (
+      <div>
+         {
+          data.map((x)=>{
+            return <h1>{x.name}</h1>
+          })
+         }
+        
+      </div>
+   
   );
 }
+
 
 export default App;
---
+++ src/list.js
@@ -0,0 +1,16 @@
+        const list =  [{
+        id:1,
+        name : "name1"
+        },
+        {
+            id:2,
+            name : "name2"
+        },
+        {
+            id:1,
+            name : "name3"
+        }
+
+         ]
+
+export default list 
